711debfd9564ce06886329300b397bc84502b4b6
Raja <gkrajareddy@gmail.com>
2020-04-17 01:29:57 +0530

    Updates

---
+++ precache-manifest.c45684c127ae4b944c22ffcd2969880c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01fba9c546d02d9e3a3d4b6f3b32c8cd",
+    "url": "/sravya_resume/index.html"
+  },
+  {
+    "revision": "67ba10429a6b2df13b7d",
+    "url": "/sravya_resume/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6a661e08e886d5ede6a2",
+    "url": "/sravya_resume/static/js/2.fc759bb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sravya_resume/static/js/2.fc759bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67ba10429a6b2df13b7d",
+    "url": "/sravya_resume/static/js/main.d4420e45.chunk.js"
+  },
+  {
+    "revision": "eb385ba9bc81377bddb3",
+    "url": "/sravya_resume/static/js/runtime-main.07dc00f1.js"
+  }
+]);
